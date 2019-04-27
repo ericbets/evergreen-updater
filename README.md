f1548e23c26660c4c99087c2f63c0272660f89fc
Eric Faccer <eric@gregarious.com.au>
2019-04-27 10:29:43 +1000

    Initial

---
+++ README.md
@@ -0,0 +1,20 @@
+Evergreen-updater
+===================
+A simple and powerful git based auto updater for node.js projects.
+
+How it works
+------------
+Chuck a git repository somewhere on the web. Server done. Tell Evergreen the local project folder to update and the remote branch you want eg. "production". Client done. This project will fetch and checkout the remote branch and rollback to the previous version if anything fails. Don't worry if the remote branch is completely different and not fast-forward mergable with what's there currently, Evergreen will handle it and still give you an efficient sync. So it's up to you to decide how often the updater is run and what remote branch to checkout. For example, you could put it in a cronjob or run it from your app.
+
+Usage
+-------
+```js
+const Evergreen = require('evergreen');
+const url = 'https://github.com/ericbets/evergreen';
+const project = './proj';
+const success = new Evergreen(url).update(project, 'production');
+```
+
+Credits
+-------
+Thanks [isomorphic-git](https://github.com/isomorphic-git/isomorphic-git), you make the world a better place. 
---
+++ evergreen.js
@@ -0,0 +1,48 @@
+const fs = require('fs-extra');
+const os = require('os');
+const path = require('path');
+const git = require("isomorphic-git");
+git.plugins.set('fs', fs);
+
+class Evergreen {
+	constructor(url) {
+		this.url = url;
+	}
+
+	async update(dir, branch) {
+		if (!fs.existsSync(dir + "/.git")) {
+			try {
+				await git.clone({dir: dir, url: this.url, ref: branch, singleBranch: true, depth: 1});
+				await git.config({ dir: dir, path: 'evergreen.count', value: 0});
+				await git.branch({dir: dir, ref: "evergreen-" + branch + "-" + 0, checkout: true});
+			}
+			catch(error) {
+				console.error(error);
+				fs.removeSync(dir + "/.git");
+				return false;
+			}
+		}
+		else {
+			let count;
+			try {
+				count = await git.config({dir:dir, path: 'evergreen.count'});
+				count = Number.parseInt(count);
+				await git.fetch({dir: dir,url:this.url, ref: branch, singleBranch: true, remote: 'origin', tags: false});
+				count = count+1;
+				await git.branch({dir: dir, ref: "evergreen-" + branch + "-" + count, checkout: true});
+				await git.config({ dir: dir, path: 'evergreen.count', value: count});
+			}
+			catch(error) {
+				console.error(error);
+				console.error("Error, rolling back..");
+				let value = await git.config({dir:dir, path: 'evergreen.count'});
+				await git.checkout({dir, ref: "evergreen-" + branch + "-" + count-1});
+				return false;
+			}
+			
+		}
+		return true;
+			
+	}
+}
+module.exports = Evergreen;
